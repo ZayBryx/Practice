0e6672c4c7a81773b3e2c925e2a0f4bca5c01066
Zybryx <montinola.zybryx.s.bsit@gmail.com>
2024-04-03 07:17:04 +0800

    login route

--- controller/accountController.js
+++ controller/accountController.js
@@ -1,38 +1,40 @@
-const Joi = require("joi");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../Errors/custom-error");
 const Account = require("../models/Account");
-
-const registerSchema = Joi.object({
-  username: Joi.string().min(3).max(30).required().messages({
-    "string.min": "Username must be at least 3 characters long",
-    "any.required": "Username is required",
-    "string.max": "Username must not exceed to 30 chracters long",
-  }),
-  email: Joi.string().email().required().messages({
-    "string.email": "Please fill a valid email address",
-    "any.required": "Email is requried",
-  }),
-  password: Joi.string()
-    .pattern(
-      new RegExp(
-        "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\\-_=+{};:,<.>ยง?|]).{8,}$"
-      )
-    )
-    .required()
-    .messages({
-      "string.pattern.base":
-        "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one digit, and one special character",
-      "any.required": "Password is required",
-    }),
-});
+const { registerValidator } = require("../validators");
+const EXPIRES = 1 * 60 * 60 * 1000; // 8 hours
 
 const login = async (req, res) => {
-  res.send("login route");
+  const { username, password } = req.body;
+
+  const account = await Account.findOne({
+    $or: [{ username: username }, { email: username }],
+  });
+
+  if (!account) {
+    throw new CustomError("User not found", StatusCodes.NOT_FOUND);
+  }
+
+  const isMatch = await account.isPasswordCorrect(password);
+
+  if (!isMatch) {
+    throw new CustomError("password is incorrect");
+  }
+  const accessToken = account.generateAccessToken();
+  const refreshToken = account.generateRefreshToken();
+
+  res
+    .status(StatusCodes.OK)
+    .cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      secure: process.env.SECURE,
+      maxAge: EXPIRES,
+    })
+    .json({ username: account.username, accessToken: accessToken });
 };
 
 const register = async (req, res) => {
-  const { value, error } = registerSchema.validate(req.body, {
+  const { value, error } = registerValidator.validate(req.body, {
     abortEarly: false,
   });
 
@@ -40,8 +42,10 @@ const register = async (req, res) => {
     throw new CustomError(error.message, StatusCodes.BAD_REQUEST);
   }
 
-  const { username, email, password } = value;
+  const { username, email, password, repeat_password } = value;
+
   const account = await Account.create({ username, email, password });
+
   res.status(StatusCodes.CREATED).json(account);
 };
 
