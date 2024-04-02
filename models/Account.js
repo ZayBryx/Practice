10b9b8f898764c6519423d6241c9b56e7c69d192
Zybryx <montinola.zybryx.s.bsit@gmail.com>
2024-04-03 06:00:43 +0800

    fixing the syntax error of unique

--- models/Account.js
+++ models/Account.js
@@ -6,13 +6,13 @@ const AccountSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, "Username is required"],
-    unique: [true, "Username is already taken"],
+    unique: true,
     minLength: [3, "Username must be minimum 3 characters"],
   },
   email: {
     type: String,
     required: [true, "email is required"],
-    unique: [true, "Email is already taken"],
+    unique: true,
     trim: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
